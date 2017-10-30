e134572a1695cb87a1b4191ea7cfbaa9c109f1e1
jl <jl@axin.fr>
2017-10-30 14:03:57 +0100

    model v1 ok

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,2 +1,21 @@
-// Todo: Create Artist Model
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AlbumSchema = require("./album");
+
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: String,
+  labelName: String,
+  retired: Boolean,
+  Albums: [AlbumSchema]
+});
+
+const artiste = mongoose.model("artiste", ArtistSchema);
+
+module.exports = artiste;
