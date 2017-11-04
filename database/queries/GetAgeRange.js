1e1fe3152e6bda0413a4bd2b6ad56d62ffeed03b
Jerome Lalisse <jerome.lalisse@hotmail.fr>
2017-11-04 11:56:29 +0100

    get range

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -1,4 +1,4 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Finds the lowest and highest age of artists in the Artist collection
@@ -6,4 +6,14 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  return Promise.all([
+    Artist.find()
+      .sort({ age: 1 })
+      .limit(1),
+    Artist.find()
+      .sort({ age: -1 })
+      .limit(1)
+  ]).then(result => {
+    return { min: result[0][0].age, max: result[1][0].age };
+  });
 };
